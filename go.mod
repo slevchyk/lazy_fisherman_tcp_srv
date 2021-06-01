21240a8a5d9134b64c754df634d2a0cec23fca51
Serhii Levchyk <s.levchyk@gmail.com>
2021-06-01 21:37:58 +0300

    init tcp server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/slevchyk/lazy_fisherman_tcp_srv
+
+go 1.14
---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"io"
+	"net"
+)
+
+func main() {
+
+	l, err := net.Listen("tcp", ":8002")
+	if err != nil {
+		panic(err)
+	}
+	defer l.Close()
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			panic(err)
+		}
+
+		for {
+			bs := make([]byte, 1024)
+			n, err := conn.Read(bs)
+			if err != nil {
+				break
+			}
+
+			request := string(bs[:n])
+
+			if request == "close" {
+				conn.Close()
+				break
+			}
+
+			answer := request + "-x\n"
+
+			io.WriteString(conn, answer)
+		}
+
+		conn.Close()
+	}
+
+}
